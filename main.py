dda3d9e24862b32de29ada0d799aec1cab94f5ec
lilalunex <alexanderscharow@icloud.com>
2025-04-25 14:13:44 +0200

    CH3 L1

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 from stats import get_books_words
 from stats import get_books_chars
+from stats import sort
 
 path_to_file = "books/frankenstein.txt"
+words_in_book_dic = {}
+words_in_book_list = []
+
+def print_char_count_one():
+    print(f"--------- Character Count -------")
+    for key, value in words_in_book_dic.items():
+        print(f"'{key}': {value}")
+
+def print_char_count_two(list_to_print):
+    print(f"--------- Character Count -------")
+    for x in list_to_print:
+        if x['char'].isalpha():
+            print(f"{x['char']}: {x['num']}")
 
 def main():
+    global words_in_book_dic
+    global words_in_book_list
     print(f"============ BOOKBOT ============")
     print(f"Analyzing book found at {path_to_file}...")
     get_books_words(path_to_file)
-    get_books_chars(path_to_file)
+    words_in_book = get_books_chars(path_to_file)
+    #print_char_count_one()
+    words_in_book_list = sort(words_in_book)
+    print_char_count_two(words_in_book_list)
+
     print(f"============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -5,8 +5,6 @@ def get_books_words(path_to_file):
         print(f"----------- Word Count ----------")
         print(f"Found {len(num_words_array)} total words")
 
-def sort_on(dict):
-    return dict["key"]
 
 def get_books_chars(path_to_file):
     with open(path_to_file) as f:
@@ -18,7 +16,17 @@ def get_books_chars(path_to_file):
                 dic[char] += 1
             else:
                 dic[char] = 1
-        dic.sort(reverse=True, key=dic[value])
-        print(f"--------- Character Count -------")
-        for key, value in dic.items():
-            print(f"'{key}': {value}")
+    return dic
+
+
+def sort_on(dic):
+    return dic["num"]
+
+
+def sort(dic):
+    sorted_list = []
+    for key, value in dic.items():
+        temp_dic = {"char": key, "num": value}
+        sorted_list.append(temp_dic)
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
